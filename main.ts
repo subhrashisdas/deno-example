4b157ddf1c74d75994733f0f4e8b9446b0c530c2
Subhrashis Das <das.subhrashis@gmail.com>
2024-10-15 11:24:02 +0530

    Added sample codebase

---
+++ main.ts
@@ -0,0 +1,10 @@
+// @deno-types="npm:@types/express@4.17.15"
+import express from "npm:express@4.18.2";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Dinosaur API!");
+});
+
+app.listen(8000);
